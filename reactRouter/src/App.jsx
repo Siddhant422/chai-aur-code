3c7116cd6bb4464fb1255fcca93329fee22840aa
Siddhant Keshari <siddhantkeshari76@gmail.com>
2024-01-29 23:37:49 +0530

    Project 7 Commit

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='bg-orange-500'>Siddhant Keshari</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
