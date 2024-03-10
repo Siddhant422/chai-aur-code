a2acdf61cdd0f40f1b9feb0f6d67c051aa2be806
Siddhant Keshari <siddhantkeshari76@gmail.com>
2024-03-10 16:30:30 +0530

    Redux Done

---
+++ reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import AddToDo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+const App = () => {
+  return (
+    <div>
+      <AddToDo />
+      <Todos />
+    </div>
+  );
+};
+
+export default App;
---
+++ reduxtoolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,26 @@
+//state: It will give us the current value of the state
+//action: It will trigger the state to do the requried function
+//Slice : It breaks down the methods into smaller functions
+//nanoId: It only provides unique id
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = { id: nanoid(), text: action.payload };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
